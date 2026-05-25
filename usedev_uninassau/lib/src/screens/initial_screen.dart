4d85e4c5d1085f658d25dc0dc2edab8a6b0a2863
yuri cruz <yuricruz063@gmail.com>
2026-05-25 19:45:42 -0300

    configurando a initial scrren para que os produtos apareçam

--- usedev_uninassau/lib/src/screens/initial_screen.dart
+++ usedev_uninassau/lib/src/screens/initial_screen.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:usedev_uninassau/src/widgets/hero_section_widget.dart';
 import 'package:usedev_uninassau/src/widgets/product_card_widget.dart';
 import 'package:usedev_uninassau/src/widgets/subscription_section_widget.dart';
+import 'package:usedev_uninassau/src/services/product_service.dart';
+import 'package:usedev_uninassau/src/models/product_model.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -12,14 +14,17 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
+
+  final _productService = ProductService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu, size: 40),
+        leading: const Icon(Icons.menu, size: 40),
         title: Image.asset('assets/logo_usedev.png', height: 40),
         centerTitle: true,
-        actions: [
+        actions: const [
           Icon(Icons.person_outline, size: 40),
           SizedBox(width: 10),
           Icon(Icons.shopping_cart_outlined, size: 40),
@@ -29,28 +34,53 @@ class _InitialScreenState extends State<InitialScreen> {
       body: SingleChildScrollView(
         child: Column(
           spacing: 20,
-          crossAxisAlignment: .stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch, 
           children: [
-            HeroSectionWidget(),
+            const HeroSectionWidget(),
             Text(
               'Promos Especiais',
-              textAlign: .center,
+              textAlign: TextAlign.center, 
               style: TextStyle(
                 fontSize: 28,
-                fontWeight: .bold,
+                fontWeight: FontWeight.bold,
                 fontFamily: GoogleFonts.orbitron().fontFamily,
               ),
             ),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: 5,
-              itemBuilder: (context, index) => ProductCardWidget(
-                nome: 'Produto 0$index',
-                url: 'https://placehold.co/600x600.png',
-                preco: '10$index,00',
-              ),
+
+            FutureBuilder<List<ProductModel>>(
+              future: _productService.getAllProducts(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } 
+                else if (snapshot.hasError) {
+                  return Center(child: Text('Erro ao carregar promos: ${snapshot.error}'));
+                } 
+
+                else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('Nenhuma promo disponível.'));
+                }
+
+             
+                final produtos = snapshot.data!;
+
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(), 
+                  itemCount: produtos.length,
+                  itemBuilder: (context, index) {
+                    final produto = produtos[index];
+                    return ProductCardWidget(
+                      nome: produto.title,
+                      url: produto.image,
+                      preco: produto.price.toStringAsFixed(2), 
+                    );
+                  },
+                );
+              },
             ),
-            SubscriptionSectionWidget(),
+            
+            const SubscriptionSectionWidget(),
           ],
         ),
       ),
